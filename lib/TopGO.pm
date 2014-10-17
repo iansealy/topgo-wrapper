1f052559baf7239dda4a83489c037b150c86aa5a
Ian Sealy <ian.sealy@sanger.ac.uk>
2014-10-17 10:12:55 +0100

    topGO wrapper repository.

---
+++ lib/TopGO.pm
@@ -0,0 +1,21 @@
+## no critic (RequireUseStrict, RequireUseWarnings, RequireTidyCode)
+package TopGO;
+## use critic
+
+# ABSTRACT: TopGO wrapper
+
+## Author         : is1
+## Maintainer     : is1
+## Created        : 2014-10-16
+## Last commit by : $Author$
+## Last modified  : $Date$
+## Revision       : $Revision$
+## Repository URL : $HeadURL$
+
+use warnings;
+use strict;
+use autodie;
+use Carp;
+use Try::Tiny;
+
+1;
