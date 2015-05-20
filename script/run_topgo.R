9728ba2d7693f2b5779c0bf2f7265e0939efcb0a
Ian Sealy <ian.sealy@sanger.ac.uk>
2015-05-20 23:39:55 +0100

    Suppress warnings whilst making plots.

--- script/run_topgo.R
+++ script/run_topgo.R
@@ -42,9 +42,11 @@ write.table( allRes, file=paste0(outputPrefix, ".all.tsv"), quote=FALSE,
 
 # Write PDF
 pdf(paste0(outputPrefix, ".pdf"))
-showSigOfNodes(GOdata, score(resultKS.elim), firstSigNodes=5, useInfo="all")
-showSigOfNodes(GOdata, score(resultKS.elim), firstSigNodes=10, useInfo="all")
-showSigOfNodes(GOdata, score(resultKS.elim), firstSigNodes=nrow(sigRes),
-    useInfo="all")
-lapply(sigRes[,1], function(x) showGroupDensity(GOdata, x))
+suppressWarnings(showSigOfNodes(GOdata, score(resultKS.elim), firstSigNodes=5,
+    useInfo="all"))
+suppressWarnings(showSigOfNodes(GOdata, score(resultKS.elim), firstSigNodes=10,
+    useInfo="all"))
+suppressWarnings(showSigOfNodes(GOdata, score(resultKS.elim),
+    firstSigNodes=nrow(sigRes), useInfo="all"))
+suppressWarnings(lapply(sigRes[,1], function(x) showGroupDensity(GOdata, x)))
 dev.off()
